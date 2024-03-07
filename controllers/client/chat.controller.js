9f18f404a69a620ae7cd2f32dd59885bb7cf6756
user020603 <ptit.tienthanh@gmail.com>
2024-03-07 09:48:01 +0700

    Typing

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,46 +1,59 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
 
-// [GET] /chat
+// [GET] /chat/
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
   const fullName = res.locals.user.fullName;
+
   // SocketIO
   _io.once("connection", (socket) => {
-    // User send messenge to server
+    // Người dùng gửi tin nhắn lên server
     socket.on("CLIENT_SEND_MESSAGE", async (content) => {
       const chat = new Chat({
         user_id: userId,
         content: content
-      })
+      });
+
       await chat.save();
 
-      socket.emit("SERVER_SEND_MESSAGE", {
+      // Trả data ra giao diện realtime
+      _io.emit("SERVER_SEND_MESSAGE", {
         userId: userId,
         fullName: fullName,
         content: content
-      })
-    })
-    // console.log("SocketIO connected:", socket.id);
-  })
+      });
+    });
+
+    // Typing
+    socket.on("CLIENT_SEND_TYPING", (type) => {
+      socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        userId: userId,
+        fullName: fullName,
+        type: type
+      });
+    });
+  });
   // End SocketIO
 
+  // Lấy data từ database
   const chats = await Chat.find({
     deleted: false
-  })
+  });
 
   for (const chat of chats) {
     const infoUser = await User.findOne({
-      _id: chat.user_id,
+      _id: chat.user_id
     }).select("fullName");
 
     chat.infoUser = infoUser;
-
   }
-  // console.log(chats)
+
+  // console.log(chats);
+  // Hết Lấy data từ database
 
   res.render("client/pages/chat/index", {
-    pageTitle: "Messenger",
+    pageTitle: "Chat",
     chats: chats
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,30 +1,43 @@
-import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js";
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js';
 
-// CLIENT SEND MESSAGE
+// CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-if (formSendData) {
+if(formSendData) {
   const inputContent = formSendData.querySelector("input[name='content']");
   formSendData.addEventListener("submit", (event) => {
     event.preventDefault();
     const content = inputContent.value;
-    console.log(content);
-    if (content) {
+    if(content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       inputContent.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
-// End CLIENT SEND MESSAGE
+// End CLIENT_SEND_MESSAGE
 
-// SERVER SEND MESSAGE
+var timeOut;
+
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+
+  clearTimeout(timeOut);
+
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+}
+
+// SERVER_SEND_MESSAGE
 socket.on("SERVER_SEND_MESSAGE", (data) => {
   const body = document.querySelector(".chat .inner-body");
+  const elementListTyping = body.querySelector(".inner-list-typing");
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
 
   const div = document.createElement("div");
   let htmlFullName = "";
 
-  if (myId != data.userId) {
+  if(myId != data.userId) {
     div.classList.add("inner-incoming");
     htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
   } else {
@@ -32,43 +45,84 @@ socket.on("SERVER_SEND_MESSAGE", (data) => {
   }
 
   div.innerHTML = `
-        ${htmlFullName}
-        <div class="inner-content">${data.content} </div>
-    `;
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
 
-  body.appendChild(div);
+  body.insertBefore(div, elementListTyping);
   body.scrollTop = body.scrollHeight;
-});
-// End SERVER SEND MESSAGE
+})
+// End SERVER_SEND_MESSAGE
 
-// Scroll Chat to bottom
+// Scroll Chat To Bottom
 const bodyChat = document.querySelector(".chat .inner-body");
-if (bodyChat) {
+if(bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
-// End Scroll Chat to bottom
+// End Scroll Chat To Bottom
 
 // Show Icon Chat
-const buttonIcon = document.querySelector(".button-icon");
-if (buttonIcon) {
-  const tooltip = document.querySelector(".tooltip");
+const buttonIcon = document.querySelector('.button-icon');
+
+if(buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
   Popper.createPopper(buttonIcon, tooltip);
 
   // Show Tooltip
   buttonIcon.addEventListener("click", () => {
-    tooltip.classList.toggle("shown");
+    tooltip.classList.toggle('shown');
   });
 
-  // Insert Icon to input
+  // Insert Icon To Input
   const emojiPicker = document.querySelector("emoji-picker");
-  const inputChat = document.querySelector(
-    ".chat .inner-form input[name='content']"
-  );
-  console.log(inputChat);
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
 
-  emojiPicker.addEventListener("emoji-click", (e) => {
-    const icon = e.detail.unicode;
-    inputChat.value += icon;
+  // Inser Icon
+  emojiPicker.addEventListener('emoji-click', event => {
+    const icon = event.detail.unicode;
+    inputChat.value = inputChat.value + icon;
+    const end = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+    showTyping();
+  });
+
+  // Show Typing
+  inputChat.addEventListener("keyup", () => {
+    showTyping();
   });
 }
 // End Show Icon Chat
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-body .inner-list-typing");
+
+socket.on("SERVER_RETURN_TYPING", (data) => {
+  if(data.type == "show") {
+    const existTyping = elementListTyping.querySelector(`.box-typing[user-id="${data.userId}"]`);
+
+    if(!existTyping) {
+      const boxTyping = document.createElement("div");
+      boxTyping.classList.add("box-typing");
+      boxTyping.setAttribute("user-id", data.userId);
+      boxTyping.innerHTML = `
+        <div class="inner-name">${data.fullName}</div>
+        <div class="inner-dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      `;
+  
+      elementListTyping.appendChild(boxTyping);
+      bodyChat.scrollTop = bodyChat.scrollHeight;
+    }
+  } else {
+    const boxTypingRemove = elementListTyping.querySelector(`.box-typing[user-id="${data.userId}"]`);
+
+    if(boxTypingRemove) {
+      elementListTyping.removeChild(boxTypingRemove);
+    }
+  }
+});
+// End SERVER_RETURN_TYPING
